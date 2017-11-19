c0a29a13fad588a228e48bfa457b98d0b30a71c8
Xuan Gong <xgong@apache.org>
2017-11-18 21:15:10 -0800

    User binary cross entropy instead of sigmoid_cross_entropy_with_logits

--- Net/Classifer_Net_Loss.py
+++ Net/Classifer_Net_Loss.py
@@ -7,7 +7,8 @@ class ClassiferNetLoss(object):
         self.config = config
 
     def getLoss(self, output, output_each, labels, isnod, batch_size, k_size):
-        loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels[:, 0], logits=output))
+        #loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels[:, 0], logits=output))
+        loss2 = tf.reduce_mean(-tf.reduce_sum(labels[:, 0] * tf.log(output), reduction_indices=[1]))
         missMask = tf.cast(output_each<self.config['miss_thresh'], dtype=tf.float32)
         missLoss = -tf.reduce_sum(missMask*isnod*tf.log(output_each+0.001))/batch_size/k_size
         return loss2+self.config['miss_ratio']*missLoss

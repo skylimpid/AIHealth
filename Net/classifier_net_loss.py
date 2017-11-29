5cd759970c91709dd288595a1e33c66a52df6996
luhuicode <eruditedata@outlook.com>
2017-11-29 00:05:50 -0800

    fix classifier loss bug

--- Net/classifier_net_loss.py
+++ Net/classifier_net_loss.py
@@ -10,9 +10,9 @@ class ClassifierNetLoss(object):
     def getLoss(self, output, output_each, labels, isnod, batch_size, k_size):
         #loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=output))
         output = tf.clip_by_value(output, clip_value_min=1e-5, clip_value_max=(1-(1e-5)))
-        loss2 = -tf.reduce_sum(labels * tf.log(1e-5+output) + (1-labels) * tf.log(1e-5+ 1 - output))
+        loss2 = -tf.reduce_sum(labels * tf.log(output) + (1-labels) * tf.log(1 - output))
         missMask = tf.cast(output_each<self.config['miss_thresh'], dtype=tf.float32)
-        missLoss = -tf.reduce_sum(missMask*isnod*tf.log(output_each+0.001))/batch_size/k_size
+        missLoss = -tf.reduce_sum(missMask*isnod*tf.log(output_each+1e-5))/batch_size/k_size
         #missLoss = -tf.reduce_sum(tf.multiply(tf.multiply(missMask, isnod), tf.log(output_each + 0.001))) / batch_size / k_size
         return tf.add(loss2, self.config['miss_ratio']*missLoss), loss2, missLoss, missMask
 
@@ -28,9 +28,9 @@ if __name__ == "__main__":
     X = tf.placeholder(tf.float32, shape=[None, topK, 1, 96, 96, 96])
     coord = tf.placeholder(tf.float32, shape=[None, topK, 3, 24, 24, 24])
 
-    net1 = DetectorNet()
-    net2 = ClassifierNet(net1)
-    nodulePred, casePred, casePred_each = net2.get_classifier_net(X, coord)
+    detector_net = DetectorNet()
+    cl_net = ClassifierNet(detector_net)
+    nodulePred, casePred, casePred_each, centerFeat, noduleFeat = cl_net.get_classifier_net(X, coord)
     print(loss_object.getLoss(casePred, casePred_each, labels, isnod, batch_size, topK))
 
     with tf.Session() as sess:
